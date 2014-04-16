fa16ee2ad3863de66de6d9d1b88690179e90f880
Daniel Xu <dxu429@gmail.com>
2014-04-16 11:41:09 -0700

    getting friends links by chunks

--- routes/index.js
+++ routes/index.js
@@ -25,19 +25,22 @@ exports.loggedin = function(req, res) {
 	var query = {
 		links: "SELECT url FROM link WHERE owner IN (SELECT uid2 FROM friend where uid1=me())"
 	};
+	// this is gonna take a long time, maybe put into another function for ajax req.
 	auth.graph.get("/me/friends", {access_token: auth.graph.getAccessToken()}, function(err, facebookRes) {
 		var friends = facebookRes.data;
-		var ids = [];
-		for (var i = friends.length - 1; i >= 0; i--) {
-			//console.log(friends[i].id);
-			ids.push(friends[i].id);
-		};
-		ids = ids.join(",");
-		console.log(ids);
-		auth.graph.fql("select url from link where owner in ("+ids+") limit 100", function(fberr, fbRes) {
+		var i, j, chunkOfIds, chunkSize = 10;
+		for(i=0; i<friends.length; i+= chunkSize) {
+			var ids;
+			
+			console.log(ids);
+		}
+		//console.log(ids);
+		/*
+		auth.graph.fql("select url from link where owner in ("+ids+") limit 100 ", function(fberr, fbRes) {
 			console.log(fbRes);
 			res.render('index', {loggedIn: 1, data: fbRes.data});
 		});
+		*/
 	});
 
 }
